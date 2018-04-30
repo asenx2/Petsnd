b6d930ad2669eee6be7cf1986da31daa701b66c9
asenx2 <alumno.304000@ies-azarquiel.es>
2018-04-30 19:00:50 +0200

    Commit finish
    App terminada. Falta comprimir imagen

--- app/src/main/java/com/asenjo/petsnd/Fragments/Misdatos.kt
+++ app/src/main/java/com/asenjo/petsnd/Fragments/Misdatos.kt
@@ -1,19 +1,14 @@
 package com.asenjo.petsnd.Fragments
 
-import android.app.Application
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import com.asenjo.petsnd.Views.Info
 import com.asenjo.petsnd.R
-import com.asenjo.petsnd.Views.Login
-import com.google.firebase.auth.FirebaseAuth
 
 
 class Misdatos : Fragment() {
@@ -21,8 +16,6 @@ class Misdatos : Fragment() {
     companion object {
         var TAG = "***tagmisdatos***"
     }
-
-    private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,31 +26,14 @@ class Misdatos : Fragment() {
         // Inflate the layout for this fragment
         val rootView = inflater!!.inflate(R.layout.activity_misdatos, container, false)
 
-        //para coger el boton dentro de un fragmento hay que hacerlo mediante findViewById
-        //si lo hago mediante las extension de synthetic la activity no se abre
-        val btnout = rootView.findViewById(R.id.btnout) as Button
-        val tvmimail = rootView.findViewById(R.id.tvmimail) as TextView
-        val tvmialta = rootView.findViewById(R.id.tvmialta) as TextView
+        val tvInfo = rootView.findViewById(R.id.tvInfo) as TextView
 
-        tvmimail.text = mAuth!!.currentUser!!.email!!.toString()
-        tvmialta.text = "ace musho"
-
-        btnout.setOnClickListener(View.OnClickListener {
-            //signout()
-            Toast.makeText(activity,"Quietoo",Toast.LENGTH_SHORT).show()
+        tvInfo.setOnClickListener(View.OnClickListener {
+            val intent = Intent(context, Info::class.java)
+            this.startActivity(intent)
         })
 
         return rootView
-    }
-
-
-
-    //funcion para cerrar sesion y volver a la pantalla inicial
-    private fun signout() {
-        mAuth!!.signOut()
-        //para obtener el contexto funciona activity y context en lugar de this
-        val intent = Intent(context,Login::class.java)
-        startActivity(intent)
     }
 
 }
---
+++ app/src/main/java/com/asenjo/petsnd/Views/Info.kt
@@ -0,0 +1,13 @@
+package com.asenjo.petsnd.Views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.asenjo.petsnd.R
+
+class Info : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
